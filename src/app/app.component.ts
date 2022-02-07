b11626c5ffc981cd1fd6a266bdc95461ce78039e
takuya kido <takuyakido1993@gmail.com>
2022-02-07 22:09:36 +0900

    done until No.89

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,14 @@
 // 外部import
 import { Component } from '@angular/core';
-import {
-  AngularFireDatabase,
-  AngularFireList,
-  SnapshotAction,
-} from '@angular/fire/database';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-// 内部import
-import { Comment } from './class/comment';
-import { User } from './class/user';
-
-const CURRENT_USER: User = new User(1, '城戸 拓也');
-const ANOTHER_USER: User = new User(2, '中村 勇斗');
 
 @Component({
   selector: 'ac-root',
-  templateUrl: './app.component.html',
+  template: `
+    <ac-header></ac-header>
+    <div class="page">
+      <router-outlet></router-outlet>
+    </div>
+  `,
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  comments$: Observable<Comment[]>;
-  commentsRef: AngularFireList<Comment>;
-  currentUser = CURRENT_USER;
-  comment = '';
-  item$: Observable<any>;
-
-  constructor(private db: AngularFireDatabase) {
-    this.item$ = db.object('/item').valueChanges();
-    this.commentsRef = db.list('/comments');
-    this.comments$ = this.commentsRef.snapshotChanges().pipe(
-      map((snapshots: SnapshotAction<Comment>[]) => {
-        return snapshots.map((snapshot) => {
-          const value = snapshot.payload.val();
-          return new Comment({ key: snapshot.payload.key, ...value });
-        });
-      })
-    );
-  }
-
-  addComment(comment: string): void {
-    if (comment) {
-      this.commentsRef.push(
-        new Comment({ user: this.currentUser, message: comment })
-      );
-      this.comment = '';
-    } else {
-      alert('コメントを入力してください!');
-    }
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
 // 外部import
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AngularFireModule } from '@angular/fire';
 
 // 内部import
 import { AppComponent } from './app.component';
-import { CommentDatePipe } from './pipes/comment-date.pipe';
-import { environment } from 'src/environments/environment';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { ChatComponent } from './chat/chat.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, CommentDatePipe],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule,
-    AngularFireDatabaseModule,
-  ],
+  declarations: [AppComponent, ChatComponent],
+  imports: [BrowserModule, SharedModule, CoreModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
