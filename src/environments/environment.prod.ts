3430fca26c3f5a4de8ab206db6a9803df8999465
takuya kido <takuyakido1993@gmail.com>
2022-02-09 11:55:25 +0900

    done deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCHBie8dRLGN1NUPLCAkmQHVVndb94CRek',
+    authDomain: 'angular-chat-ebe01.firebaseapp.com',
+    projectId: 'angular-chat-ebe01',
+    storageBucket: 'angular-chat-ebe01.appspot.com',
+    messagingSenderId: '970849910700',
+    appId: '1:970849910700:web:dce3f59267e5efa6aa8409',
+  },
 };
